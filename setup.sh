1dfa28683ba1d526712517f693e6a51569bba8c6
Chris Kjellqvist <chriskjellqvist@gmail.com>
2022-10-21 17:34:04 -0400

    fix setup a little more

--- setup.sh
+++ setup.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 branch=master
-prefix=https://www.github.com/
+# prefix=https://www.github.com/
+prefix=git@github.com
 
 
 
@@ -31,6 +32,7 @@ echo "branch is $branch"
 git clone -q "$prefix"ChrisKjellqvist/Composer-Hardware.git && cd Composer-Hardware && git checkout -q $branch && chmod u+x setup.sh && ./setup.sh && cd ..
 git clone -q "$prefix"ChrisKjellqvist/Composer-Software.git && cd Composer-Software && git checkout -q $branch && cd ..
 git clone -q "$prefix"ChrisKjellqvist/Composer_Verilator.git && cd Composer_Verilator && git checkout -q $branch && cd ..
+git clone -q "$prefix"ChrisKjellqvist/Composer-Examples.git
 
 echo ""
 echo "==========================================================================="
