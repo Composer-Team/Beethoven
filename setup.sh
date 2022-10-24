f08f63b50be6fab525da5ad3e802db32159e2c37
Chris Kjellqvist <chriskjellqvist@gmail.com>
2022-10-24 15:27:20 -0400

    small push

--- setup.sh
+++ setup.sh
@@ -29,10 +29,10 @@ done
 
 echo "branch is $branch"
 
-git clone -q "$prefix"ChrisKjellqvist/Composer-Hardware.git && cd Composer-Hardware && git checkout -q $branch && chmod u+x setup.sh && ./setup.sh && cd ..
-git clone -q "$prefix"ChrisKjellqvist/Composer-Software.git && cd Composer-Software && git checkout -q $branch && cd ..
-git clone -q "$prefix"ChrisKjellqvist/Composer_Verilator.git && cd Composer_Verilator && git checkout -q $branch && cd ..
-git clone -q "$prefix"ChrisKjellqvist/Composer-Examples.git
+git clone -q "$prefix":ChrisKjellqvist/Composer-Hardware.git && cd Composer-Hardware && git checkout -q $branch && chmod u+x setup.sh && ./setup.sh && cd ..
+git clone -q "$prefix":ChrisKjellqvist/Composer-Software.git && cd Composer-Software && git checkout -q $branch && cd ..
+git clone -q "$prefix":ChrisKjellqvist/Composer_Verilator.git && cd Composer_Verilator && git checkout -q $branch && cd ..
+git clone -q "$prefix":ChrisKjellqvist/Composer-Examples.git
 
 echo ""
 echo "==========================================================================="
