c429edd3ea75922ac8b0c6074d01246135723274
Chris Kjellqvist <chriskjellqvist@gmail.com>
2023-02-07 01:21:49 -0500

    update setup

--- install/setup.sh
+++ install/setup.sh
@@ -3,16 +3,17 @@
 branch=master
 # prefix=https://www.github.com/
 prefix=git@github.com:
+if [ -z "${COMPOSER_ROOT-}" ]; then
+	echo "You must define environment variable COMPOSER_ROOT before running setup. This is where your Composer project will live."
+	exit
+fi	
 
+cp -r bin $COMPOSER_ROOT/
+cd $COMPOSER_ROOT
 
-# Do everything in root directory
-cd ..
-
-echo "branch is $branch"
-
-git clone -q "$prefix"Composer-Team/Composer-Hardware.git && cd Composer-Hardware && git checkout -q $branch && chmod u+x scripts/setup.sh && ./scripts/setup.sh && cd ..
-git clone -q "$prefix"Composer-Team/Composer-Software.git && cd Composer-Software && git checkout -q $branch && cd ..
-git clone --recursive -q "$prefix"Composer-Team/Composer-Runtime.git && cd Composer-Runtime && git checkout -q $branch && cd ..
+git clone -q "$prefix"Composer-Team/Composer-Hardware.git && cd Composer-Hardware && chmod u+x scripts/setup.sh && ./scripts/setup.sh && cd ..
+git clone -q "$prefix"Composer-Team/Composer-Software.git && cd Composer-Software && cd ..
+git clone --recursive -q "$prefix"Composer-Team/Composer-Runtime.git && cd Composer-Runtime cd ..
 git clone -q "$prefix"Composer-Team/Composer-Examples.git
 
 echo ""
