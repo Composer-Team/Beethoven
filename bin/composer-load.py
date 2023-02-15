462e67dcca8ea59b455a925b3e5715a1d5ef57a6
Chris Kjellqvist <chriskjellqvist@gmail.com>
2023-02-15 09:24:44 -0500

    more scripts

--- bin/composer-load.py
+++ bin/composer-load.py
@@ -1,14 +1,32 @@
 #!/usr/bin/python3
 
 import os
-import sys
 import json
-import subprocess
+
+# Make sure that Composer directory exists
+if os.environ.get('COMPOSER_ROOT') is None:
+    print("You must define the $COMPOSER_ROOT environment variable. We expect it to be the top-level of the"
+          " Composer repo.")
+    exit(1)
+
+# Make sure that xdma is setup and working, otherwise set it up
+xdmas = list(os.walk("/dev"))[0][2]
+if len(list(filter(lambda x: "xdma" in x, xdmas))) == 0:
+    os.system(f"cd {os.environ['COMPOSER_ROOT']} && git clone --recursive https://github.com/aws/aws-fpga.git"
+              f" && cd aws-fpga/linux_kernel_drivers/xdma/ && make && sudo insmod xmda.ko")
 
 # aws ec2 describe-fpga-images --owner self
 proc = os.popen("aws ec2 describe-fpga-images --owner self")
-images = json.load(proc)
-#print(images['FpgaImages'])
-print([i['Name'] for i in images['FpgaImages']])
+images = json.load(proc)['FpgaImages']
+names = [i['Name'] for i in images]
+
+for idx, nm in enumerate(names):
+    print(f"\t[{idx}] - {nm}")
+
+choice = int(input("Select an image to load\n"))
+assert len(names) > choice >= 0
+os.system("sudo fpga-load-image -S 0 -I " + str(images[choice]['FpgaImageGlobalId']))
+
+
 
 
