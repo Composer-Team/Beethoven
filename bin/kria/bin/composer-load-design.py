32e84988f54da3b60a193e19f7850010b0001f92
Chris Kjellqvist <chriskjellqvist@gmail.com>
2023-02-18 13:25:04 -0500

    kria stuff

--- bin/kria/bin/composer-load-design.py
+++ bin/kria/bin/composer-load-design.py
@@ -18,7 +18,8 @@ lib_d = f"/lib/firmware/xilinx/{chosen_name}"
 os.system(f"sudo mkdir -p {lib_d} && sudo cp {chosen_d}/{chosen_name}.dtbo {lib_d}/ && "
           f"sudo cp {chosen_d}/{chosen_name}.bit.bin {lib_d}/ && "
           f"mkdir -p {os.environ['COMPOSER_ROOT']}/Composer-Hardware/vsim/generated-src/ && "
-          f"cp {chosen_d}/composer_allocator_declaration.h {os.environ['COMPOSER_ROOT']}/Composer-Hardware/vsim/generated-src/")
+          f"cp {chosen_d}/composer_allocator_declaration.h {os.environ['COMPOSER_ROOT']}/Composer-Hardware/vsim/generated-src/ && "
+          f"sudo cp {os.environ['HOME']}/bin/default_shell.json {lib_d}")
 
 os.system(f"sudo dfx-mgr-client -remove && sudo dfx-mgr-client -load {chosen_name}")
 os.system(f"cd {os.environ['COMPOSER_ROOT']}/Composer-Runtime/ && mkdir -p build && "
