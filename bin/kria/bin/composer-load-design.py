16776c295a45a7ab0d8efa33fced26843b742f4b
Chris Kjellqvist <chriskjellqvist@gmail.com>
2023-02-18 13:29:54 -0500

    fix shell

--- bin/kria/bin/composer-load-design.py
+++ bin/kria/bin/composer-load-design.py
@@ -19,7 +19,7 @@ os.system(f"sudo mkdir -p {lib_d} && sudo cp {chosen_d}/{chosen_name}.dtbo {lib_
           f"sudo cp {chosen_d}/{chosen_name}.bit.bin {lib_d}/ && "
           f"mkdir -p {os.environ['COMPOSER_ROOT']}/Composer-Hardware/vsim/generated-src/ && "
           f"cp {chosen_d}/composer_allocator_declaration.h {os.environ['COMPOSER_ROOT']}/Composer-Hardware/vsim/generated-src/ && "
-          f"sudo cp {os.environ['HOME']}/bin/default_shell.json {lib_d}")
+          f"sudo cp {os.environ['HOME']}/bin/default_shell.json {lib_d}/shell.json")
 
 os.system(f"sudo dfx-mgr-client -remove && sudo dfx-mgr-client -load {chosen_name}")
 os.system(f"cd {os.environ['COMPOSER_ROOT']}/Composer-Runtime/ && mkdir -p build && "
