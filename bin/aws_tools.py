f77139c43e9411bab0b37f543d271918d43aa410
Chris Kjellqvist <chriskjellqvist@gmail.com>
2023-06-27 13:15:48 -0400

    update scripts

--- bin/aws_tools.py
+++ bin/aws_tools.py
@@ -1,22 +1,10 @@
 import os
 from typing import List
-from enum import Enum
+from VerilogUtils import *
 
+HOME = os.environ['HOME']
+EncryptTCLfname = HOME + "/bin/aws/src/encrypt.tcl"
 
-HOME=os.environ['HOME']
-
-
-# This can change from time to time, so define here and use enum instead within code
-class InterfacePrefixes:
-    slave = "S"
-    master = "M"
-    DMA = "dma"
-
-
-class PortClass(Enum):
-    Slave = 0
-    Master = 1
-    DMA = 2
 
 def get_class(name: str):
     if name[:len(InterfacePrefixes.slave)] == InterfacePrefixes.slave:
@@ -27,271 +15,6 @@ def get_class(name: str):
         return PortClass.DMA
     else:
         raise Exception
-
-
-def is_number(q):
-    # noinspection PyBroadException
-    try:
-        a = int(q)
-    except:
-        return False
-    return True
-
-
-def extract(s, p):
-    assert p in s
-    idx = s.find(p)
-    return s[:idx] + s[idx + len(p):]
-
-
-class Wire:
-    def __init__(self, name, width, ar_width):
-        self.name = name
-        self.width = width
-        self.ar_width = ar_width
-        self.occupancy = 0
-        self.assignment = []
-
-    def __hash__(self):
-        return self.name.__hash__()
-
-    def assign(self, g, operand):
-        assert self.occupancy < self.ar_width
-        assert operand.ar_width == 1
-        self.assignment.append(operand)
-        assert self.ar_width == 1
-        op1 = self.name
-        # Dont' overassign
-        if operand.width < self.width:
-            op2 = '{' + f"{self.width - operand.width}'b0, {operand.name}" + "}"
-        elif self.width < operand.width:
-            op2 = f"{operand.name}[{self.width - 1}:0]"
-        else:
-            op2 = operand.name
-
-        g.write(f"assign {op1} = {op2};\n")
-        self.occupancy += 1
-
-    def assign_array(self, g, op):
-        g.write(f"assign {self.name} = {op.name};\n")
-
-    def get_array_subwire(self, i):
-        if self.ar_width > 1:
-            return Wire(f"{self.name}[{i}]", self.width, 1)
-        else:
-            return Wire(f"{self.name}[{i}]", 1, 1)
-
-    def __eq__(self, other):
-        return self.name == other.name
-
-    def is_ddr_pin(self):
-        return 'sh_cl' in self.name or 'cl_sh' in self.name
-
-    def tie_off(self, g, special_val="0"):
-        g.write(f"assign {self.name} = ")
-        tieoff = "0"
-        if special_val == '1':
-            tieoff = '1' * self.width
-        if self.ar_width == 1:
-            g.write(f"{self.width}'b{tieoff};\n")
-        else:
-            g.write("'{")
-            for i in range(self.ar_width):
-                g.write(f"{self.width}'b{tieoff}, ")
-            g.write("};\n")
-
-    def __and__(self, other):
-        assert other.ar_width == 1 and self.ar_width == 1
-        assert other.width == self.width
-        return Wire(f"({self.name} & {other.name})", self.width, 1)
-
-
-class VerilogPort(Wire):
-    def __init__(self, name: str, width: int, ar_len: int, io_type: str, is_logic: bool):
-        super().__init__(name, width, ar_width=ar_len)
-        if 'bits_' in name:
-            idx = name.find('bits_')
-            self.axi_clean_name = name[:idx] + name[idx + len('bits_'):]
-        else:
-            self.axi_clean_name = name
-        self.input = io_type == 'input'
-        self.output = io_type == 'output'
-        self.inout = io_type == 'inout'
-        self.logic = is_logic
-        assert self.input or self.output or self.inout
-
-    def get_group_name(self):
-        if not '_' in self.axi_clean_name:
-            return None
-        else:
-            return self.axi_clean_name[:self.axi_clean_name.find('_')]
-
-    def get_axi_part_name(self):
-        return self.axi_clean_name.split('_')[-1]
-
-    def is_stat(self):
-        return 'ddr' in self.axi_clean_name and 'stat' in self.axi_clean_name
-
-    def get_stat_name(self):
-        return self.axi_clean_name.split('_')[-1]
-
-    def __hash__(self):
-        return self.name.__hash__()
-
-    def assign(self, g, operand):
-        assert self.output
-        super().assign(g, operand)
-
-    def assign_constant(self, g, val_str):
-        assert self.output
-        super().assign(g, Wire(val_str, self.width, self.ar_width))
-
-
-class Reg(Wire):
-    def assign(self, g, operand: Wire):
-        assert self.occupancy < self.ar_width
-        assert operand.ar_width == 1
-        self.assignment.append(operand)
-        assert self.ar_width == 1
-        op1 = self.name
-        # Dont' overassign
-        if operand.width < self.width:
-            op2 = '{' + f"{self.width - operand.width}'b0, {operand.name}" + "}"
-        elif self.width < operand.width:
-            op2 = f"{operand.name}[{self.width - 1}:0]"
-        else:
-            op2 = operand.name
-        g.write(f"always_ff @(posedge clk)\n"
-                f"begin\n"
-                f"\t{op1} <= {op2};\n"
-                f"end\n")
-        self.occupancy += 1
-
-
-def scrape_ports_from_lines(lns):
-    ports = []
-    lns_fresh = map(lambda x: x.strip().replace(',', '').replace('wire', ''), lns)
-    lns_filt = []
-    filter_stack = []
-
-    def can_consume():
-        return '/*' not in filter_stack and '`ifdef' not in filter_stack
-
-    for ln in lns_fresh:
-        print(ln)
-        found_comment = False
-        if '/*' in ln:
-            filter_stack.append('/*')
-            ln = ln[ln.find('/*')+2:]
-            found_comment = True
-            print('d')
-        if '*/' in ln:
-            assert filter_stack[-1] == '/*'
-            filter_stack = filter_stack[:-1]
-            found_comment = False
-            print('e')
-        if found_comment:
-            continue
-        if '`ifdef' in ln:
-            filter_stack.append('`ifdef')
-            print('c')
-            continue
-        if '`ifndef' in ln:
-            filter_stack.append('`ifndef')
-            print('b')
-            continue
-        if '`endif' in ln:
-            assert filter_stack[-1] == '`ifndef' or filter_stack[-1] == '`ifdef'
-            filter_stack = filter_stack[:-1]
-            print('a')
-            continue
-        if '`define' in ln:
-            print("found define")
-            continue
-
-        if not can_consume():
-            print(f"skipping {ln}")
-            continue
-        if ");" in ln:
-            print("ending reading, found " + ln)
-            break
-        lns_filt.append(ln)
-        pass
-
-    print('')
-    for ln in lns_filt:
-        lncopy = ln
-        if 'NUM_GTY' in ln:
-            raise Exception("AHH")
-        # remove everything after comments
-        if '//' in ln:
-            ln = ln[:ln.find('//')].strip()
-        # if it's empty, remove now
-        if ln == '':
-            continue
-        if ");" in ln:
-            return ports
-        if 'input' in ln:
-            io_ty = 'input'
-            ln = extract(ln, 'input')
-        elif 'output' in ln:
-            io_ty = 'output'
-            ln = extract(ln, 'output')
-        elif 'inout' in ln:
-            io_ty = 'inout'
-            ln = extract(ln, 'inout')
-        else:
-            raise Exception("Unrecognized port type in " + ln)
-
-        is_logic = 'logic' in ln
-        if is_logic:
-            ln = extract(ln, 'logic')
-
-        name_str = str(ln.replace('input', '').replace('output', '').strip())
-        found_start = False
-        found_end = False
-        name_start = -1
-        name_end = -1
-        for i, c in enumerate(name_str):
-            c = str(c)
-            if not found_start:
-                if c.isalpha():
-                    found_start = True
-                    name_start = i
-                    continue
-            elif found_start and not found_end:
-                if not c.isalnum() and c != '_':
-                    found_end = True
-                    name_end = i
-                    continue
-        if name_end != -1:
-            name = name_str[name_start:name_end]
-        else:
-            name = name_str[name_start:]
-
-        # Find width
-        before_name = name_str[:name_start]
-        if '[' in before_name:
-            n = before_name[before_name.find('[') + 1:before_name.find(':')]
-            if not is_number(n):
-                n = 0
-            width = 1 + int(n)
-        else:
-            width = 1
-
-        # Find ar_width
-        after_name = name_str[name_end:]
-        if '[' in after_name:
-            ar_width = 1 + int(after_name[after_name.find('[') + 1:after_name.find(':')])
-        else:
-            ar_width = 1
-        # if 'bits_' in name:
-        #     name = name[:name.find('bits_')] + name[name.find('bits_') + 5:]
-
-        if 'rst_' in name or 'reset' in name or 'clk' in name or 'clock' in name:
-            continue
-        ports.append(VerilogPort(name, width, ar_width, io_ty, is_logic))
-    return ports
 
 
 def scrape_aws_ports():
@@ -321,9 +44,6 @@ def scrape_cl_ports():
     return scrape_ports_from_lines(lns)
 
 
-
-
-
 def scrape_sh_ddr_ports():
     with open(f"{HOME}/aws-fpga/hdk/common/shell_stable/design/sh_ddr/sim/sh_ddr.sv") as f:
         lns = f.readlines()[45:]
@@ -350,7 +70,6 @@ def search_for_part(part, prefix, part_list: List[VerilogPort]):
     matches = []
     for port in filter(lambda x: prefix in x.name, part_list):
         if "_" + part == port.name[-len(part) - 1:]:
-            print(f"{part} matches in {port.name}")
             matches.append(port)
     return matches
 
@@ -417,9 +136,6 @@ def create_aws_shell():
     cl_ios: List[VerilogPort] = scrape_cl_ports()
     shell_ports: List[VerilogPort] = scrape_aws_ports()
 
-    for pr in cl_ios:
-        print(f"name: {pr.name}")
-
     to_tie = []
 
     g = open("composer_aws.sv", 'w')
@@ -477,7 +193,7 @@ def create_aws_shell():
             shell_sig_reg.assign(g, search_for_part("is_ready", "ddr_", shell_ports)[0])
             creadys.append(shell_sig_reg)
         if ndram > 1:
-            for i in range(ndram-1):
+            for i in range(ndram - 1):
                 shell_sig_reg = declare_reg_with_name(g, f"RESERVED_SH_DDR_is_ddr_ready{i}", 1, 1)
                 shell_sig_reg.assign(g, ddr_trained_ddrsig.get_array_subwire(i))
                 creadys.append(shell_sig_reg)
@@ -524,13 +240,11 @@ def create_aws_shell():
 
             ddr_axis.update({axi_name: ddr_fuse})
             if cl_mems.get(axi_name) is None and ddr.input:
-                print(f"Skipping part {ddr.name}. We think it's an AXI3 interface...")
                 continue
             # Find parts to bind to from CL / shell
             ports = cl_mems[ddr.get_axi_part_name()]
             assert ports is not None
             if len(ports) == 0:
-                print("no ports found matching " + axi_name)
                 break
             # we use shell ports first (only sh_ddr for 2nd 3rd 4th dimm)
 
@@ -670,7 +384,6 @@ def create_aws_shell():
         if pwire.name[:2] == 'M_' or lower.find('ddr') != -1 \
                 or lower.find('ocl') != -1 or lower.find('clk') != -1 or lower.find('rst') != -1:
             continue
-        print(lower)
         if 'ack' in lower:
             set_to = "1"
         else:
@@ -694,14 +407,16 @@ def write_id_defines():
                 "`define CL_SH_ID1 32'h1D51_FEDC\n")
 
 
-def write_encrypt_script_from_base_inline(fname, ):
-    with open(fname) as f:
+def write_encrypt_script():
+    with open(EncryptTCLfname) as f:
         lns = f.readlines()
+
     to_write = ["file copy -force $CL_DIR/design/composer_aws.sv $TARGET_DIR\n",
-                "file copy -force $CL_DIR/design/cl_id_defines.vh $TARGET_DIR\n"] +\
+                "file copy -force $CL_DIR/design/cl_id_defines.vh $TARGET_DIR\n"] + \
                [f"file copy -force $CL_DIR/design/{x} $TARGET_DIR\n"
                 for x in list(os.walk(f"{HOME}/build-dir/generated-src/"))[0][2]]
-    with open(fname, 'w') as f:
+
+    with open("build/scripts/encrypt.tcl", 'w') as f:
         for ln in lns:
             if "file copy" in ln:
                 if to_write is not None:
@@ -715,25 +430,22 @@ def write_encrypt_script_from_base_inline(fname, ):
                 f.write(ln)
 
 
-def create_synth_script(oname):
-    with open(f"{HOME}/aws-fpga/hdk/cl/examples/cl_dram_dma/build/scripts/"
-              f"synth_cl_dram_dma.tcl") as f:
-        lns = f.readlines()
-    with open(oname, 'w') as g:
-        for ln in lns:
-            if "glob $ENC_SRC" in ln:
-                g.write(f"read_verilog -sv [glob {os.getcwd()}/design/*v]")
-            elif "*.?v" in ln:
-                idx = ln.find("*.?v")
-                g.write(ln[:idx] + "*v" + ln[idx + 4:])
-            elif "close_project" in ln:
-                g.write("report_power -file power_report.txt\nreport_utilization -hierarchical -file utilization_report.txt\n" + ln)
-            else:
-                g.write(ln)
+def create_synth_script(srcs):
+    with open(f"{HOME}/bin/aws/src/synth.tcl") as i:
+        lns = i.readlines()
+        whole_file = ""
+        for q in lns:
+            whole_file = whole_file + q
+
+    with open("build/scripts/synth.tcl", 'w') as o:
+        src_list = ""
+        for src in srcs:
+            src_list = src_list + f"\t{src} \\"
+        whole_file.format(vlist=src_list)
 
 
-def create_dcp_script_inline(fname):
-    os.system(f"sed -i.bu 's/cl_hello_world/composer_aws/g' {fname}")
+def copy_dcp_scripts():
+    os.system(f"cp {HOME}/bin/aws/src/*dcp* build/scripts/")
 
 
 def move_sources_to_design():
